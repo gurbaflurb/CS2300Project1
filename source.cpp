2f254f93fb26d27e12c629f8c0bbbeae63121cfe
gurbaflurb <seachen4411@gmail.com>
2019-09-11 02:49:36 -0600

    Adjusted source.cpp and makefile, removed executable

--- source.cpp
+++ source.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <tuple>
 #include <vector>
 
-int main()
+std::tuple<int, int, std::vector<int>> readData(std::string filePath)
 {
+    std::vector<int> matrix;
     std::fstream file;
     std::string line;
+    int rows;
+    int columns;
+    file.open(filePath);
 
-    file.open("Amatrix");
-
-    if(!file.is_open())
+    if(file.is_open())
     {
-        while(getline(file, line, ' '))
+        std::getline(file, line, ' ');
+        rows = stoi(line);
+        std::getline(file, line, ' ');
+        columns = stoi(line);
+
+        for (int i = 0; i < columns; i++)
         {
-            std::cout << line << std::endl;
+            for(int j = 0; j < rows; j++)
+            {
+                std::getline(file, line, ' ');
+                matrix.push_back(stoi(line));
+            }
         }
     }
     else
     {
-        std::cout << "Failed to open file!\nQuitting!" << std::endl;    
+        std::cout << "Failed to open file!\nQuitting!" << std::endl; 
     }
     file.close();
-    
+    return std::make_tuple(rows, columns, matrix);
+}
+
+int main()
+{
+    int row1, row2, column1, column2;
+    std::vector<int> matrix1;
+    std::vector<int> matrix2;
+
+    std::tie(row1, column1, matrix1) = readData("Amatrix");
+    std::tie(row2, column2, matrix2) = readData("Bmatrix");
+
+    for(int i = 0; i < column1; i++)
+    {
+        for(int j = 0; j < row1; j++)
+        {
+            std::cout << matrix1[i+j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    for(int i = 0; i < column2; i++)
+    {
+        for(int j = 0; j < row2; j++)
+        {
+            std::cout << matrix2[i+j] << ", ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
